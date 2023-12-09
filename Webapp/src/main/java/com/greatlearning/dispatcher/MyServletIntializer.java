c93e6042eeaa19bf6df2737aae4930195f656940
nitin-gl <142153440+nitin-gl@users.noreply.github.com>
2023-12-10 04:48:55 +0530

    Add files via upload

---
+++ Webapp/src/main/java/com/greatlearning/dispatcher/MyServletIntializer.java
@@ -0,0 +1,28 @@
+package com.greatlearning.dispatcher;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import com.greatlearning.config.SpringConfig;
+
+public class MyServletIntializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[0];
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		// TODO Auto-generated method stub
+		return new Class[] {SpringConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		// TODO Auto-generated method stub
+		return new String[] {"/"};
+	}
+	
+
+}
