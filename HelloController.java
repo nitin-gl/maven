f862ca7f6d1b2beacb637aea60c1bfcbbd89f1c4
nitin-gl <142153440+nitin-gl@users.noreply.github.com>
2023-12-09 18:10:02 +0530

    Add files via upload

---
+++ HelloController.java
@@ -0,0 +1,17 @@
+package com.greatlearning.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	public String giveWelcomeMessage(ModelMap model) {
+
+		model.addAttribute("message", "Spring MVC thymeleaf Hello World");
+		return "Welcome";
+
+	}
+}
